3f0689f21093b671e9a0347c7a48424f40115092
jkalar <46872976+jkalar@users.noreply.github.com>
2020-10-09 09:44:36 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 /* Javascript by Jeff Kalar */
 
-var mapND = document.getElementById("mapND");
-mapND.innerHTML = "Jeff Sucks!";
+var mapND = L.map('mapid').setView([47.589199, -100.337102], 7);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(mapND);
+
