cee3be21195d6f1626d9f4907086fd1fc25b0a73
jkalar <46872976+jkalar@users.noreply.github.com>
2020-10-09 12:16:07 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ $.getJSON("data/citiesND.geojson")
 console.log(data);
 });
 
+// Function to process data which acquires the year columns and min/max population values
+
 function processData(data) {
     var timestamps = [];
     var min = Infinity;
@@ -50,4 +52,50 @@ function processData(data) {
         max : max
     }
 }
+
+// Function to map the proportional symbols
+
+function createPropSymbols(timestamps, data) {
+    
+    cities = L.geoJson(data, {
+        
+        pointToLayer: function(feature, LatLng) {
+            return L.circleMarker(LatLng, {
+                fillColor: "#501e65",
+                color: '#501e65',
+                weight: 3,
+                fillOpacity: 0.7
+            }).on({
+                mouseover: function(e) {
+                    this.openPopup();
+                    this.setStyle({fillColor: 'green'});
+                },
+                mouseout: function(e) {
+                    this.closePopup();
+                    this.setStyle({fillColor: '#501e65'});
+                }
+            });
+        }
+    }).addTo(mapND);
+    
+    updatePropSymbols(timestamps[0]);
+}
+
+// Function to resize each marker according to value
+
+function updatePropSymbols(timestamp) {
+    
+    cities.eachLayer(function(layer) {
+        
+        var props = layer.feature.properties;
+        var radius = calcPropRadius(props[timestamp]);
+        
+        var popupContent = props.name + ' ' + timestamp + ' population: ' + String(props[timestamp]) ;
+        
+        layer.setRadius(radius);
+        layer.bindPopup(popupContent, { offset: new L.Point(0,-radius) });
+    });
+}
+
+// Calculate radius of symbols based on their area
 
